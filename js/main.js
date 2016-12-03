5768e665b46b6a00cdc9c67d1db474702a89f9bf
heoyunjee <heoyunjee@gmail.com>
2016-12-03 22:09:49 +0900

    Refactor main page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,70 @@
 (function(global, $) {
   'use strict';
 
+  var header_carousel_count = {
+    'count': 1
+  };
+  var main_box_office_carousel = {
+    'count': 1,
+    'index': {
+      'desktop': 2,
+      'tablet': 1,
+      'mobile': 0
+    }
+  };
+  var main_movie_ranking_carousel = {
+    'count': {
+      'desktop': 5,
+      'tablet': 3,
+      'mobile': 2
+    }
+  };
+
+  var main_movie_trailers_carousel = {
+    'count': {
+      'desktop': 3,
+      'tablet': 2,
+      'mobile': 1
+    }
+  };
+
+
+  if(global.innerWidth <= 750) {
+    // Count for Mobile
+    main_movie_ranking_carousel.count = main_movie_ranking_carousel.count.mobile;
+    main_movie_trailers_carousel.count = main_movie_trailers_carousel.count.mobile;
+    // Index fot mobile
+    main_box_office_carousel.index = main_box_office_carousel.index.mobile;
+  } else if(global.innerWidth <= 1024) {
+    // Count
+    main_movie_ranking_carousel.count = main_movie_ranking_carousel.count.tablet;
+    main_movie_trailers_carousel.count = main_movie_trailers_carousel.count.tablet;
+    // Index
+    main_box_office_carousel.index = main_box_office_carousel.index.tablet;
+  } else {
+    // Count
+    main_movie_ranking_carousel.count = main_movie_ranking_carousel.count.desktop;
+    main_movie_trailers_carousel.count = main_movie_trailers_carousel.count.desktop;
+    // Index
+    main_box_office_carousel.index = main_box_office_carousel.index.desktop;
+  }
+
+  var $caro4 = $('#main-header-carousel').fingoCarousel({
+    'width': 1640,
+    'height': 850,
+    'infinite': false
+  });
+
   var $caro = $('#main-box-office-carousel').fingoCarousel({
     'height': 530,
-    'index': 2,
+    'index': main_box_office_carousel.index,
     'col': 5,
     'margin': 20
   });
 
   var $caro2 = $('#main-movie-ranking-carousel').fingoCarousel({
     'height': 690,
-    'count': 5,
+    'count': main_movie_ranking_carousel.count,
     'col': 5,
     'row': 2,
     'margin': 20,
@@ -19,21 +73,11 @@
 
   var $caro3 = $('#main-new-movie-trailers-carousel').fingoCarousel({
     'height': 225,
-    'count': 3,
+    'count': main_movie_trailers_carousel.count,
     'col': 3,
     'margin': 20,
     'infinite': false
   });
-
-  var $caro4 = $('#main-header-carousel').fingoCarousel({
-    'width': 1640,
-    'height': 850,
-    'infinite': false
-  });
-
-  global.$caro = $caro;
-  global.$caro2 = $caro2;
-  global.$caro2 = $caro3;
 
   var main_movie_ranking_btn_group = document.querySelector('.main-movie-ranking-btn-group');
   var active_btns = main_movie_ranking_btn_group.querySelectorAll('button');
@@ -45,6 +89,8 @@
   });
 
   var tabpanels = document.querySelectorAll('.ui-carousel-tabpanel');
+  var tabs = document.querySelectorAll('.ui-carousel-tab');
+  console.log(tabs);
 
   tabpanels.forEach(function(tabpanel, index) {
     var tabpanel_btns = tabpanel.querySelectorAll('button');
@@ -63,4 +109,12 @@
       }
     });
   });
+
+  // 인디케이터 기본 이벤트 차단
+  tabs.forEach(function(tab) {
+    tab.addEventListener('click', function(e) {
+      if(e) { e.preventDefault(); }
+    });
+  });
+
 })(this, this.jQuery);
